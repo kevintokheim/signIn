748d66ef045f59134fb71ff2601a9cb6dc24d369
Kevin Tokheim <katokh89@gmail.com>
2015-09-26 18:29:25 -0500

    fix bugs.

--- services/StudentsFactory.js
+++ services/StudentsFactory.js
@@ -1,10 +1,10 @@
 signIn.factory("StudentsFactory", function StudentsFactory(){
     var factory = {};
 
-    $scope.students = [
+    factory.students = [
         { name:"Brentwood Smith", signedIn: false },
         { name: "Karl Klingenberg", signedIn: false },
-        { name: "Sam 'Sammy' Samwise", signedIn: false},
+        { name: "Sam Sammy Samwise", signedIn: false},
         { name: "Frodo Bagginsberg", signedIn: false },
         { name: "Sven Svensgardsen", signedIn: false }
     ];
