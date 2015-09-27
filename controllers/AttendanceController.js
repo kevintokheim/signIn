b61f91cfceb393269e41613732a898f7da9906e4
Kevin Tokheim <katokh89@gmail.com>
2015-09-27 00:07:30 -0500

    add signStudentsOut function and and ng-show/ng-hide capabilities to show the sign out button when the sign in button is clicked and vice versa.

--- controllers/AttendanceController.js
+++ controllers/AttendanceController.js
@@ -1,10 +1,4 @@
 signIn.controller('AttendanceCtrl', function AttendanceCtrl($scope, StudentsFactory){
     $scope.students = StudentsFactory.students;
     $scope.StudentsFactory = StudentsFactory;
-    $scope.signStudentsIn = function(student){
-        student.signedIn = true;
-    };
-
-    console.log($scope.students.signedIn);
-
 });
--- controllers/StudentsController.js
+++ controllers/StudentsController.js
@@ -1,5 +1,12 @@
 signIn.controller("StudentsCtrl", function StudentsCtrl($scope, StudentsFactory){
     $scope.students = StudentsFactory.students;
     $scope.StudentsFactory = StudentsFactory;
+    $scope.signStudentsIn = function(student){
+        student.signedIn = true;
+        console.log(student.signedIn);
+    };
 
+    $scope.signStudentsOut = function(student){
+        student.signedIn = false;
+    };
 });
