eb297e17fbab502d3b7d9b98156c38d5578c9de5
Kevin Tokheim <katokh89@gmail.com>
2015-09-26 22:59:02 -0500

    fix bug to allow students to display on attendance.html page.

--- controllers/AttendanceController.js
+++ controllers/AttendanceController.js
@@ -1,10 +1,10 @@
 signIn.controller('AttendanceCtrl', function AttendanceCtrl($scope, StudentsFactory){
     $scope.students = StudentsFactory.students;
     $scope.StudentsFactory = StudentsFactory;
-    $scope.SignStudentsIn = function(students){
-        students.signedIn = true;
+    $scope.signStudentsIn = function(student){
+        student.signedIn = true;
     };
 
-    console.log(factory.students.signedIn);
+    console.log($scope.students.signedIn);
 
 });
